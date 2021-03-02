9412592ac00d7e7f871344f9cfd96ac213a58e86
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-02 17:26:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Blaise User Backup Pipeline
+
+Azure devops pipeline for backing up Blaise users from the SQLite DB (`Credentials.db`)
+
+Requires:
+variable group name (created by TF)
+environment (created manually)
---
+++ scripts/BackUpDatabase.ps1
@@ -0,0 +1,5 @@
+﻿###############################
+# Backup Users pipeline script
+###############################
+
+sqlite3\sqlite3 $env:DatabaseFileLocation ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
