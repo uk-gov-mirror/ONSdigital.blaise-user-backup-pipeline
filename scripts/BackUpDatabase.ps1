55f128aa4cd3f0f0afc744164245eb0b728851fb
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-02 17:55:24 +0000

    write vars

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -2,7 +2,7 @@
 # Backup Users pipeline script
 ###############################
 
-Write-Host "VARS Write-Host: ($env:DatabaseFileLocation)"
+Write-Host "VARS Write-Host: ($env:ENV_DATABASE_FILE_LOCATION)"
 Write-Host "HEllo "
 
-scripts/tools/sqlite3.exe $env:DatabaseFileLocation ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
+scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
