862a4e55dd89c4f5cadfcf32d02855dc8aaec035
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-02 17:34:42 +0000

    rename

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -2,4 +2,4 @@
 # Backup Users pipeline script
 ###############################
 
-sqlite3\sqlite3 $env:DatabaseFileLocation ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
+tools\sqlite3 $env:DatabaseFileLocation ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
