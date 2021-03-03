26780330482ca5fcbb97afceaadae68a9ad13543
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-03 10:44:11 +0000

    Add seconds to filename

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -6,5 +6,5 @@ Write-Host "VARS Write-Host: ($env:ENV_DATABASE_FILE_LOCATION)"
 Write-Host "HEllo "
 Write-Host "$ENV_BUCKET_NAME"
 
-scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
+scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm-ss").db"
 gsutil cp *.db gs://$env:ENV_BUCKET_NAME
