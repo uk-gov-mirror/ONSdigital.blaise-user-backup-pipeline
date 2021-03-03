5081204c9f2a7db33b35d8ea10716b7b3e38e23d
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-03 10:37:34 +0000

    Copy file to GCP Bucket

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -6,3 +6,4 @@ Write-Host "VARS Write-Host: ($env:ENV_DATABASE_FILE_LOCATION)"
 Write-Host "HEllo "
 
 scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm").db"
+gsutil cp *.db gs://ENV_BUCKET_NAME
