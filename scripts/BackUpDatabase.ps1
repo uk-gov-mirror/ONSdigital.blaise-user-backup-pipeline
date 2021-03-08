ef64b051f8e7a64c3a74adb4ad5c51a82585beaa
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-03-08 15:38:07 +0000

    Backup all DB Files

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -5,5 +5,15 @@
 Write-Host "Database location var: ($env:ENV_DATABASE_FILE_LOCATION)"
 Write-Host "Backup Bucket var: ($env:ENV_BLAISE_BACKUP_BUCKET)"
 
-scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION ".clone Credentials-$(Get-Date -Format "dd-MM-yyyy-HH-mm-ss").db"
-gsutil cp *.db gs://$env:ENV_BLAISE_BACKUP_BUCKET
+$date = Get-Date -Format "dd-MM-yyyy-HH:mm:ss"
+
+ForEach ($Result in $DB_FILES) {
+    Write-Host "Backing up file - $Result"
+    $fileName = $( $Result -replace '.db', $date )
+
+    scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION$Result ".clone $Result"
+
+    gsutil cp $Result gs://$env:ENV_BLAISE_BACKUP_BUCKET/$date/
+
+    Remove-Item $Result
+}
