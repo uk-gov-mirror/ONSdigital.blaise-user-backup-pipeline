328aafe54907778f505393720d4869595d4aa426
thegrew <Ali.Grew@cloudsolutions.co.uk>
2021-05-14 12:55:35 +0100

    Split subfolders further down

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -8,6 +8,9 @@ Write-Host "Backup Bucket var: ($env:ENV_BLAISE_BACKUP_BUCKET)"
 $DB_FILES = Get-ChildItem $env:ENV_DATABASE_FILE_LOCATION -file *.db
 Write-Host "Files to Backup - $DB_FILES"
 
+$year       = Get-Date -Format "yyyy"
+$month      = Get-Date -Format "MM"
+$day        = Get-Date -Format "dd"
 $date       = Get-Date -Format "dd-MM-yyyy"
 $time       = Get-Date -Format "HH:mm:ss"
 $servertime = "$(Hostname)_$time"
@@ -18,7 +21,7 @@ ForEach ($Result in $DB_FILES) {
 
     scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION$Result ".clone $Result"
 
-    gsutil cp $Result gs://$env:ENV_BLAISE_BACKUP_BUCKET/$date/$servertime/
+    gsutil cp $Result gs://$env:ENV_BLAISE_BACKUP_BUCKET/$year/$month/$day/$servertime/
 
     Remove-Item $Result
     Write-Host "-------------------------------------"
