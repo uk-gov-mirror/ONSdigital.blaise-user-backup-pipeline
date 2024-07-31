3753c7bf706981f1b65ff518f2aec0f4f749126a
Richmond Rice <richmond@ricey.org>
2024-07-31 23:19:30 +0100

    debugging

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -1,8 +1,4 @@
-﻿###############################
-# Backup Users pipeline script
-###############################
-
-Write-Host "Ensure we are authenticated with the correct service account"
+﻿Write-Host "Ensure we are authenticated with the correct service account"
 gcloud config set account $env:ENV_VM_SERVICEACCOUNT
 
 Write-Host "Database location var: ($env:ENV_DATABASE_FILE_LOCATION)"
@@ -22,10 +18,14 @@ ForEach ($Result in $DB_FILES) {
     Write-Host "Backing up file - $Result"
     $fileName = $( $Result -replace '.db', $date )
 
+    Write-Host "Cloning database file..."
     scripts/tools/sqlite3.exe $env:ENV_DATABASE_FILE_LOCATION$Result ".clone $Result"
 
+    Write-Host "Uploading file to GCS..."
+    Write-Host "Source file path: $Result"
+    Write-Host "Destination GCS path: gs://$env:ENV_BLAISE_BACKUP_BUCKET/$year/$month/$day/$servertime/"
     gsutil cp $Result gs://$env:ENV_BLAISE_BACKUP_BUCKET/$year/$month/$day/$servertime/
 
+    Write-Host "Removing temporary file..."
     Remove-Item $Result
-    Write-Host "-------------------------------------"
 }
