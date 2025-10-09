c2f676ff2ca2f24a5a8537fb7aa346135cdbff10
Richmond Rice <richmond@ricey.org>
2025-10-09 14:05:32 +0100

    use sa impersonation

--- scripts/BackUpDatabase.ps1
+++ scripts/BackUpDatabase.ps1
@@ -1,18 +1,16 @@
-﻿Write-Host "VM service account env var: ($env:ENV_VM_SERVICEACCOUNT)"
+Write-Host "VM service account env var: ($env:ENV_VM_SERVICEACCOUNT)"
 Write-Host "SQLite databases location env var: ($env:ENV_DATABASE_FILE_LOCATION)"
 Write-Host "Backup bucket env var: ($env:ENV_BLAISE_BACKUP_BUCKET)"
-
-Write-Host "Ensure we are authenticated with the correct service account"
-gcloud config set account $env:ENV_VM_SERVICEACCOUNT
 
 $DB_FILES = Get-ChildItem "$env:ENV_DATABASE_FILE_LOCATION" -file *.db
 Write-Host "Files to Backup - $DB_FILES"
 
-$year       = Get-Date -Format "yyyy"
-$month      = Get-Date -Format "MM"
-$day        = Get-Date -Format "dd"
-$time       = Get-Date -Format "HH:mm:ss"
+$year      = Get-Date -Format "yyyy"
+$month     = Get-Date -Format "MM"
+$day       = Get-Date -Format "dd"
+$time      = Get-Date -Format "HH:mm:ss"
 $servertime = "$(Hostname)_$time"
+$SA_TO_IMPERSONATE = $env:ENV_VM_SERVICEACCOUNT # Target SA email
 
 ForEach ($Result in $DB_FILES) {
     Write-Host "Backing up file - $Result"
@@ -20,10 +18,12 @@ ForEach ($Result in $DB_FILES) {
     Write-Host "Cloning database file..."
     scripts/tools/sqlite3.exe "$env:ENV_DATABASE_FILE_LOCATION$Result" ".clone '$Result'"
 
-    Write-Host "Uploading file to GCS..."
+    Write-Host "Uploading file to GCS using Service Account Impersonation..."
     Write-Host "Source file path: $Result"
     Write-Host "Destination GCS path: gs://$env:ENV_BLAISE_BACKUP_BUCKET/$year/$month/$day/$servertime/"
-    gsutil cp "$Result" "gs://$env:ENV_BLAISE_BACKUP_BUCKET/$year/$month/$day/$servertime/"
+
+    # KEY CHANGE: Using -i (impersonate-service-account) flag
+    gsutil -i $SA_TO_IMPERSONATE cp "$Result" "gs://$env:ENV_BLAISE_BACKUP_BUCKET/$year/$month/$day/$servertime/"
 
     Write-Host "Removing temporary file..."
     Remove-Item $Result
